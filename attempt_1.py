c21ba731639457d804d35e8807d8ce016a71686e
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-02-18 20:55:38 -0600

    added comments

--- attempt_1.py
+++ attempt_1.py
@@ -13,27 +13,29 @@ def read_file(file_name):
 read_file('a_example.txt')
 
 def calulateScore(cur_image, image):
-    uniqueTags = list()
-    uniqueVal = 0
+    uniqueTags = list() #list of venn diagram unique, intersect, unquie
+    uniqueVal = 0 #the number that will be added to list
     
+    #calculate the unqinue of cur_image
     for tag in cur_image:
         if not(tag in image):
             uniqueVal += 1
 
-    uniqueTags.append(uniqueVal)
-    uniqueTags.append(len(cur_image) - uniqueVal)
+    uniqueTags.append(uniqueVal) #the unique of cur_image appeneded
+    uniqueTags.append(len(cur_image) - uniqueVal) #the number of intersecting
 
     uniqueVal = 0
 
+     #calculate the unqinue of image
     for tag in image:
         if not(tag in cur_image):
             uniqueVal += 1
     
     uniqueTags.append(uniqueVal)
     
-    return min(uniqueTags)
+    return min(uniqueTags) #return the minimum of the three
 
-image1 = {"cat", "beach", "sun", "world", "grrr"}
+image1 = {"cat", "beach", "sun", "world"}
 image2 = {"cat", "garden", "beach", "hello"}
 
 print(calulateScore(image1, image2))
