7319df8bd218166c821751caafdfcc9e026fa5a0
shanemarvinmay <shanemarvinmay@gmail.com>
2020-02-18 20:57:43 -0600

    read file and make h and v done

--- attempt_1.py
+++ attempt_1.py
@@ -7,6 +7,22 @@ def read_file(file_name):
     lines = lines.split('\n')
     lines.pop(0) # getting rid of the number of images
     lines.pop(-1) # getting rid of the empty last line
-    print(lines)
+    
+    idx = 0
+    for line in lines:
+        img = line.split(' ')
+        # getting img tags
+        img_tags = set()
+        for i in range(2, len(img)):
+            img_tags.add(img[i])
+        # putting the img in either h or v
+        if img[0] == 'H':
+            h[idx] = img_tags
+        else: 
+            v[idx] = img_tags
+        idx += 1
+    return h, v
 
-read_file('a_example.txt')
+h, v = read_file('a_example.txt')
+print(h)
+print(v)
