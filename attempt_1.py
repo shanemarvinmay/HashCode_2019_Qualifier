2c13e0787bde8b7318a68b176e650c4b073bc2ac
shanemarvinmay <shanemarvinmay@gmail.com>
2020-02-18 20:44:11 -0600

    read input file into list

--- attempt_1.py
+++ attempt_1.py
@@ -1,2 +1,12 @@
 def read_file(file_name):
+    tags = {} # tags to idxs
+    h = {} # horizontal image idx to tags
+    v = {} # verticle image idx to tags
     f = open(file_name, 'r')
+    lines = f.read() #readlines was a cool method but kept the \n in every string
+    lines = lines.split('\n')
+    lines.pop(0) # getting rid of the number of images
+    lines.pop(-1) # getting rid of the empty last line
+    print(lines)
+
+read_file('a_example.txt')
