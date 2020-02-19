b8fff6fa7c3b74b5b55ef279ae1830f9b86ebdf1
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-02-18 20:42:58 -0600

    calculateScore function works

--- attempt_1.py
+++ attempt_1.py
@@ -1,2 +1,28 @@
 def read_file(file_name):
     f = open(file_name, 'r')
+
+def calulateScore(cur_image, image):
+    uniqueTags = list()
+    uniqueVal = 0
+    
+    for tag in cur_image:
+        if not(tag in image):
+            uniqueVal += 1
+
+    uniqueTags.append(uniqueVal)
+    uniqueTags.append(len(cur_image) - uniqueVal)
+
+    uniqueVal = 0
+
+    for tag in image:
+        if not(tag in cur_image):
+            uniqueVal += 1
+    
+    uniqueTags.append(uniqueVal)
+    
+    return min(uniqueTags)
+
+image1 = {"cat", "beach", "sun", "world", "grrr"}
+image2 = {"cat", "garden", "beach", "hello"}
+
+print(calulateScore(image1, image2))
