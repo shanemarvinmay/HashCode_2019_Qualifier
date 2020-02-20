c9f8e53ae6c912c1118e85223f41e14c7d80b4d7
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-02-19 20:00:30 -0600

    fix camelCasing & spacing

--- attempt_1.py
+++ attempt_1.py
@@ -23,6 +23,7 @@ def read_file(file_name):
             v[idx] = img_tags
         idx += 1
     return h, v
+
 def find_biggest_img(h, super_v):
     bg_idx = 0
     bg_tags = 0
@@ -35,96 +36,109 @@ def find_biggest_img(h, super_v):
             bg_idx = i
             bg_tags = len(super_v[i])
     return [bg_idx]
+
+def calculate_score(cur_image, image):
+    unique_tags = list() #list of venn diagram unique, intersect, unquie
+    unique_value = 0 #the number that will be added to list
+    
+    #calculate the unqinue of cur_image
+    for tag in cur_image:
+        if not(tag in image):
+            unique_value
+     += 1
+
+    unique_tags.append(unique_value) #the unique of cur_image appeneded
+    unique_tags.append(len(cur_image) - unique_value) #the number of intersecting
+
+    unique_value = 0
+
+     #calculate the unqinue of image
+    for tag in image:
+        if not(tag in cur_image):
+            unique_value
+     += 1
+    
+    unique_tags.append(unique_value)
+    
+    return min(unique_tags) #return the minimum of the three
+
+def get_dff(t1, t2):
+    return max( t1.difference(t2), t2.difference(t1) )
+
+def create_super_v(v):
+    super_v = {}
+    # getting img with most tags 
+    max_tags = 0
+    max_idx = -1
+
+    for img in v:
+        if len(v[img]) > max_tags:
+            max_tags = len(v[img])
+            max_idx = img
+
+    # finding matches with the greatest difference
+    ignore = set()
+    v_list = list(v.keys())
+    v_list.insert(0, max_idx)
+
+    for img in v_list:
+        max_diff = -1 
+        max_idx = -1
+
+        if img in ignore:
+            continue
+
+        ignore.add(img)
+
+        for jmg in v:
+            if jmg in ignore:
+                continue
+
+            diff = get_dff(v[img], v[jmg])
+
+            if diff > max_diff:
+                max_diff = diff
+                max_idx = jmg
+
+        combined_key = str(img) + '_' + str(max_idx) 
+
+        super_v[ combined_key ] = v[img]
+        super_v[ combined_key ].update(v[max_idx])
+    
+        ignore.add(max_idx)
+    
+    return super_v
+
+def create_tags(h, super_v):
+    tags = {}
+
+    for img in h:
+        for tag in h[img]:
+            if tag not in tags:
+                tags[tag] = set()
+
+            tags[tag].add(img)
+
+    for img in super_v:
+        for tag in super_v[img]:
+            if tag not in tags:
+                tags[tag] = set()
+
+            tags[tag].add(img)
+            
+    return tags
+
+
 a = 'a_example.txt'
 b = 'b_lovely_landscapes.txt'
 c = 'c_memorable_moments.txt'
 d = 'd_pet_pictures.txt'
 e = 'e_shiny_selfies.txt'
 h, v = read_file(a)
-# print(h)
-# print(v)
+
 slideshow = find_biggest_img(h, v)
-print(slideshow)
-
-def calulateScore(cur_image, image):
-    uniqueTags = list() #list of venn diagram unique, intersect, unquie
-    uniqueVal = 0 #the number that will be added to list
-    
-    #calculate the unqinue of cur_image
-    for tag in cur_image:
-        if not(tag in image):
-            uniqueVal += 1
-
-    uniqueTags.append(uniqueVal) #the unique of cur_image appeneded
-    uniqueTags.append(len(cur_image) - uniqueVal) #the number of intersecting
-
-    uniqueVal = 0
-
-     #calculate the unqinue of image
-    for tag in image:
-        if not(tag in cur_image):
-            uniqueVal += 1
-    
-    uniqueTags.append(uniqueVal)
-    
-    return min(uniqueTags) #return the minimum of the three
-
-image1 = {"cat", "beach", "sun", "world"}
-image2 = {"cat", "garden", "beach", "hello"}
-
-# print(calulateScore(image1, image2))
-
-def get_dff(t1, t2):
-    return max( t1.difference(t2), t2.difference(t1) )
-def create_super_v(v):
-    super_v = {}
-    # getting img with most tags 
-    max_tags = 0
-    max_idx = -1
-    for img in v:
-        if len(v[img]) > max_tags:
-            max_tags = len(v[img])
-            max_idx = img
-    # print("max img", v[max_idx])
-    # finding matches with the greatest difference
-    ignore = set()
-    v_list = list(v.keys())
-    v_list.insert(0, max_idx)
-    for img in v_list:
-        max_diff = -1 
-        max_idx = -1
-        if img in ignore:
-            continue
-        ignore.add(img)
-        for jmg in v:
-            if jmg in ignore:
-                continue
-            diff = get_dff(v[img], v[jmg])
-            if diff > max_diff:
-                max_diff = diff
-                max_idx = jmg
-        combined_key = str(img) + '_' + str(max_idx) 
-        super_v[ combined_key ] = v[img]
-        super_v[ combined_key ].update(v[max_idx])
-        ignore.add(max_idx)
-    return super_v
-
 super_v = create_super_v(v)
-print(super_v)
-
-def create_tags(h, super_v):
-    tags = {}
-    for img in h:
-        for tag in h[img]:
-            if tag not in tags:
-                tags[tag] = set()
-            tags[tag].add(img)
-    for img in super_v:
-        for tag in super_v[img]:
-            if tag not in tags:
-                tags[tag] = set()
-            tags[tag].add(img)
-    return tags
-
 tags = create_tags(h, v)
-# print(tags)
+
+print(super_v)
+print(slideshow)
